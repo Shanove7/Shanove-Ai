1c5a0c64f26337ae565297562157ddcdd4d57ea5
Kasan <114809701+Shanove7@users.noreply.github.com>
2026-01-08 13:30:44 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Optional jika ada css tambahan
+
+// Baris import './index.css' SUDAH SAYA HAPUS BIAR GAK ERROR
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
 
